0eb4c649d9eb9dd74bd175bc970ac0e86d054f8b
Elliot Baptist <ewnb.exe@gmail.com>
2019-03-17 22:15:31 +0000

    Add demo mode

--- RegSpinCTRL/RegSpinCTRL.ino
+++ RegSpinCTRL/RegSpinCTRL.ino
@@ -5,6 +5,29 @@
 // Constants
 
 // Functions
+int char_to_flap(char ch) {
+  int flap = -1;
+  ch = toupper(ch);
+   if ('A' <= ch && ch <= 'Z') {
+    flap = ch-'A';
+   } else {
+    switch (ch) {
+      case '?': flap = 26; break;
+      case '!': flap = 27; break;
+      case '&': flap = 28; break;
+      case '/': flap = 29; break;
+      case ',': flap = 30; break;
+      case ' ': flap = 31; break;
+    }
+  }
+  return flap;
+}
+
+// Globals
+char ch, flap;
+int demo_index;
+unsigned long last_active;
+String demo_message = "hi! demo mode, enter msg?";
 
 // Setup
 void setup() 
@@ -13,24 +36,28 @@ void setup()
   Serial.println("Booting");
 
   EWNB_RegSplitFlap::init();
+  while (!EWNB_RegSplitFlap::reachedTarget(0)) {;}
+
+  last_active = millis();
 }
 
 // Loop
 void loop() 
 {
-  int targets[] = {0};
-  EWNB_RegSplitFlap::setTargets(targets, sizeof(targets)/sizeof(targets[0]));
-  while(!EWNB_RegSplitFlap::reachedTargets()) {;}
-  delay(1000);
-  
-  for (int i=0; i<32; i++){
-    EWNB_RegSplitFlap::setTarget(i, 0);
-    while(!EWNB_RegSplitFlap::reachedTargets()) {;}
-    delay(1000);
+  flap = -1;
+  ch = Serial.read();
+  if (ch != -1) { // user message
+    flap = char_to_flap(ch);
+    last_active = millis();
+    demo_index = 0;
+  } else if (last_active+10000 < millis()) { // demo mode
+    ch = demo_message.charAt(demo_index);
+    flap = char_to_flap(ch);
+    demo_index = (demo_index + 1) % demo_message.length();
   }
-  
-//  EWNB_RegSplitFlap::setTarget(0, 0);
-//  while(!EWNB_RegSplitFlap::reachedTargets()) {;}
-//  EWNB_RegSplitFlap::setTarget(1, 0);
-//  while(!EWNB_RegSplitFlap::reachedTargets()) {;}
+  if (flap != -1) {
+     EWNB_RegSplitFlap::setTarget(flap, 0);
+     while (!EWNB_RegSplitFlap::reachedTarget(0)) {;}
+     delay(1000);
+  }
 }
